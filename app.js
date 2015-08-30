15efbf40da154945c85ffad0ca7fcc2664fad5d1
Grisha <grisharozhnov@gmail.com>
2015-08-30 21:44:36 +0300

    Add handlers

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
-//var bodyParser = require('body-parser');
+
 
 var db = mongoose.connect('mongodb://localhost/userDb');
 
@@ -13,21 +13,8 @@ var port = process.env.PORT || 3030;
 //'============ Load routes ==============';
 require('./routes')(app);
 //'============ Load routes ==============';
-/*
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 
-userRouter = require('./routes/user.js')(User);
-
-app.use('/users', userRouter);
-//app.use('/users/posts', postRouter);
-
-
-app.get('/', function(req, res){
-    res.send('welcome to my Api!');
-});
-*/
 app.listen(port, function() {
     console.log('Listening on port ' + port);
 });

d1501a3e7356335887cf0a02dfa4b363059c969d
Grisha <grisharozhnov@gmail.com>
2015-08-30 21:56:33 +0300

    Add all

---
+++ handlers/user.js
@@ -0,0 +1,75 @@
+var User = require('../models/userModel.js');
+var Userhand = function(){
+    this.create = function(req, res, next){
+            var user = new User(req.body);
+
+            user.save();
+            res.status(201).send(user);
+
+        };
+    this.getAll = function(req, res, next){
+
+            var query = {};
+
+            if(req.query.name)
+            {
+                query.name = req.query.name;
+            }
+            User.find(query, function (err, users){
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(users);
+                }
+            });
+        };
+
+    this.getById = function(req, res){
+
+            res.json(req.user);
+
+        };
+
+    this.updateUser = function(req, res){
+            req.user.name = req.body.name;
+            req.user.sourname = req.body.sourname;
+            req.user.email = req.body.email;
+            req.user.age = req.body.age;
+            req.user.save(function(err) {
+                if(err){
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.user);
+                }
+            });
+        };
+
+    this.updateParam = function(req, res){
+        if(req.body._id){
+            delete req.body._id;
+        }
+        for(var key in req.body){
+            req.user[key] = req.body[key];
+        }
+
+        req.user.save(function(err) {
+            if(err){
+                res.status(500).send(err);
+            } else {
+                res.json(req.user);
+            }
+        });
+    };
+
+    this.deleteUser = function(req, res){
+            req.user.remove(function(err){
+                if(err){
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send('User removed');
+                }
+            });
+        };
+};
+
+module.exports = Userhand;
--- routes/user.js
+++ routes/user.js
@@ -1,32 +1,9 @@
 var express = require('express');
 
 var routes = function(User){
+    var UserHandler = require('../handlers/user');
     var userRouter = express.Router();
-
-    userRouter.route('/')
-        .post(function(req, res){
-            var user = new User(req.body);
-
-            user.save();
-            res.status(201).send(user);
-
-        })
-        .get(function(req, res){
-
-            var query = {};
-
-            if(req.query.name)
-            {
-                query.name = req.query.name;
-            }
-            User.find(query, function (err, users){
-                if(err) {
-                    res.status(500).send(err);
-                } else {
-                    res.json(users);
-                }
-            });
-        });
+    var userHandler = new UserHandler();
 
     userRouter.use('/:userId', function(req, res, next) {
         User.findById(req.params.userId, function (err, user){
@@ -41,50 +18,14 @@ var routes = function(User){
 
         });
     });
-    userRouter.route('/:userId')
-        .get(function(req, res){
 
-            res.json(req.user);
+    userRouter.post('/', userHandler.create);
+    userRouter.get('/', userHandler.getAll);
+    userRouter.get('/:userId', userHandler.getById);
+    userRouter.put('/:userId',userHandler.updateUser);
+    userRouter.patch('/:userId',userHandler.updateParam);
+    userRouter.delete('/:userId',userHandler.deleteUser);
 
-        })
-        .put(function(req, res){
-            req.user.name = req.body.name;
-            req.user.sourname = req.body.sourname;
-            req.user.email = req.body.email;
-            req.user.age = req.body.age;
-            req.user.save(function(err) {
-                if(err){
-                    res.status(500).send(err);
-                } else {
-                    res.json(req.user);
-                }
-            });
-        })
-        .patch(function(req, res){
-            if(req.body._id){
-                delete req.body._id;
-            }
-            for(var key in req.body){
-                req.user[key] = req.body[key];
-            }
-
-            req.user.save(function(err) {
-                if(err){
-                    res.status(500).send(err);
-                } else {
-                    res.json(req.user);
-                }
-            });
-        })
-        .delete(function(req, res){
-            req.user.remove(function(err){
-                if(err){
-                    res.status(500).send(err);
-                } else {
-                    res.status(204).send('User removed');
-                }
-            });
-        });
     return userRouter;
 };
 
